e70ec224be46481267ed55c8f543604e7ddb0582
shia <shiashunell@gmail.com>
2025-07-10 09:52:17 +0000

    Add files via upload

---
+++ JavaSC.js
@@ -0,0 +1,17 @@
+<input type="file" id="uploadPFP" accept="image/*">
+<br><br>
+<img id="profilePic" src="" alt="Your Profile Picture" width="150" height="150" style="border-radius: 50%; display: none;" />
+
+<script>
+  const uploadInput = document.getElementById('uploadPFP');
+  const profilePic = document.getElementById('profilePic');
+
+  uploadInput.addEventListener('change', function () {
+    const file = this.files[0];
+    if (file) {
+      const imageURL = URL.createObjectURL(file);
+      profilePic.src = imageURL;
+      profilePic.style.display = 'block';
+    }
+  });
+</script>
